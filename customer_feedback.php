0f50da5eca5b9d3a6accc92ce965a8365a4c3d42
alexefitz1021 <alexefitz@gmail.com>
2020-03-03 11:46:54 -0600

    Add files via upload

---
+++ customer_feedback.php
@@ -0,0 +1,26 @@
+
+<?php
+    ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Customer Feedback</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    
+    <main>
+        <h1>Customer Feedback</h1>
+
+        <label>Your Name:</label><br>
+        <span><?php ?></span><br>
+
+        <label>Your Email:</label><br>
+        <span><?php ?></span><br>
+
+        <label>Your Feedback:</label><br>
+        <span><?php ?></span><br>
+    </main>
+</body>
+</html>
